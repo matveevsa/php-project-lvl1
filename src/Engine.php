4ffc37cbb590a4f47e3bfb33e11740c23655ba4f
Matveev S <matveevsa987@gmail.com>
2020-04-24 20:50:33 +0300

    fix

--- src/Engine.php
+++ src/Engine.php
@@ -5,7 +5,7 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function startGame($typeGame, $message, $attempts = 3)
+function startGame(&$typeGame, $message, $attempts = 3)
 {
     line('Welcome to the Brain Games!');
     line($message . "\n");
