b2ba3661f6bdc974bcd06a508747ab0691bf4f44
Matveev S <matveevsa987@gmail.com>
2020-04-24 20:12:46 +0300

    fix

--- src/Engine.php
+++ src/Engine.php
@@ -4,6 +4,8 @@ namespace BrainGames\Engine;
 
 use function cli\line;
 use function cli\prompt;
+use function BrainGames\BrainCalc\brainCalc;
+use function BrainGames\BrainEven\brainEven;
 
 function startGame($typeGame, $message, $attempts = 3)
 {
